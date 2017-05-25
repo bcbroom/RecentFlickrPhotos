41ba8ad4a4de675e7fb2f3f44cc53fe1cf8688f1
Brian Broom <brian.broom@gmail.com>
2017-05-24 22:53:43 -0400

    Drag to reorder with long press, instead of just edit button.

--- PhotoView/PhotosTableViewController.m
+++ PhotoView/PhotosTableViewController.m
@@ -31,8 +31,97 @@
     NSLog(@"Error: %@", [error localizedDescription]);
   }];
   
-  // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+  UILongPressGestureRecognizer *longPress = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(longPressGestureRecognized:)];
+  [self.tableView addGestureRecognizer:longPress];
+  
   self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (IBAction)longPressGestureRecognized:(id)sender {
+  UILongPressGestureRecognizer *longPress = (UILongPressGestureRecognizer *)sender;
+  UIGestureRecognizerState state = longPress.state;
+  
+  CGPoint location = [longPress locationInView:self.tableView];
+  NSIndexPath *indexPath = [self.tableView indexPathForRowAtPoint:location];
+  
+  static UIView *snapshot = nil;
+  static NSIndexPath *sourceIndexPath = nil;
+  
+  switch (state) {
+    case UIGestureRecognizerStateBegan: {
+      if (indexPath) {
+        sourceIndexPath = indexPath;
+        
+        UITableViewCell *cell = [self.tableView cellForRowAtIndexPath:indexPath];
+        snapshot = [self customSnapshotFromView:cell];
+        
+        __block CGPoint center = cell.center;
+        snapshot.center = center;
+        snapshot.alpha = 0.0;
+        [self.tableView addSubview:snapshot];
+        [UIView animateWithDuration:.25 animations:^{
+          center.y = location.y;
+          snapshot.center = center;
+          snapshot.transform = CGAffineTransformMakeScale(1.05, 1.05);
+          snapshot.alpha = 0.98;
+          
+          cell.alpha = 0.0;
+        } completion:^(BOOL finished) {
+          cell.hidden = YES;
+        }];
+      }
+      break;
+    }
+      
+    case UIGestureRecognizerStateChanged: {
+      CGPoint center = snapshot.center;
+      center.y = location.y;
+      snapshot.center = center;
+      
+      if (indexPath && ![indexPath isEqual:sourceIndexPath]) {
+        [self.photos exchangeObjectAtIndex:indexPath.row withObjectAtIndex:sourceIndexPath.row];
+        [self.tableView moveRowAtIndexPath:sourceIndexPath toIndexPath:indexPath];
+        sourceIndexPath = indexPath;
+      }
+      break;
+    }
+      
+    default: {
+      UITableViewCell *cell = [self.tableView cellForRowAtIndexPath:sourceIndexPath];
+      cell.hidden = NO;
+      cell.alpha = 0.0;
+      
+      [UIView animateWithDuration:0.25 animations:^{
+        
+        snapshot.center = cell.center;
+        snapshot.transform = CGAffineTransformIdentity;
+        snapshot.alpha = 0.0;
+        cell.alpha = 1.0;
+        
+      } completion:^(BOOL finished) {
+        
+        sourceIndexPath = nil;
+        [snapshot removeFromSuperview];
+        snapshot = nil;
+        
+      }];
+      
+      break;
+    }
+  }
+}
+
+- (UIView *)customSnapshotFromView:(UIView *)inputView {
+
+  UIView *snapshot = [inputView snapshotViewAfterScreenUpdates:YES];
+  
+  snapshot.layer.masksToBounds = NO;
+  snapshot.layer.cornerRadius = 0.0;
+  snapshot.layer.shadowOffset = CGSizeMake(-5.0, 0.0);
+  snapshot.layer.shadowRadius = 5.0;
+  snapshot.layer.shadowOpacity = 0.4;
+  
+  return snapshot;
 }
 
 #pragma mark - Table view data source
